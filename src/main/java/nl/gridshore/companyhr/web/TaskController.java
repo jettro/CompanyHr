f6010da88b82cb32ef1acaac7b06dc332ca35f54
jettro <jettro@gridshore.nl>
2011-06-22 10:54:31 +0200

    The google app engine specific code was moved to the axon incubator project. This project is updated to latest versions, but we have a problem with the latest axon release and google app engine. We need to overcome this before we can continue

--- src/main/java/nl/gridshore/companyhr/web/TaskController.java
+++ src/main/java/nl/gridshore/companyhr/web/TaskController.java
@@ -1,6 +1,6 @@
 package nl.gridshore.companyhr.web;
 
-import nl.gridshore.companyhr.app.axon.GaeSnapshotter;
+import org.axonframework.eventstore.gae.GaeSnapshotter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
